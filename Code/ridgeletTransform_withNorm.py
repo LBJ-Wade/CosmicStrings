aecdc312055a003ce8ca1918e4bac918706dfc5f
Felix Valin <felix.valin@mail.mcgill.ca>
2017-11-24 13:47:23 -0500

    Added comments, renamed packages

--- Code/ridgeletTransform_withNorm.py
+++ Code/ridgeletTransform_withNorm.py
@@ -4,39 +4,12 @@ Created on Fri Jun  9 19:25:14 2017
 
 @author: felix
 """
-
-
-#import matplotlib.pyplot as plt
-#import peakdetection as pd
 import numpy as np
-#import pywt
 import density3D as dens
-import cube
-import BrutForceNorm as bnorm
+import createLines as cube
+import normalization as norm
 import lineInterpolation as line
-"""
-#Data acquisition
-default_dataname = "pos(1)"
-dataname = raw_input("filename w/o '.npz' (default is '%s')?: " %(default_dataname))
-if dataname == "":
-  dataname = default_dataname
-dataname = "../Data/" + dataname
-dataname +=".npz"
 
-print "Loading %s..." %dataname
-xpos = np.load(dataname)['x']
-ypos = np.load(dataname)['y']
-zpos = np.load(dataname)['z']
-
-
-
-#Rearranges into one 2D array
-pos = np.zeros((3,len(xpos)))
-pos[0,:] = xpos[:]
-pos[1,:] = ypos[:]
-pos[2,:] = zpos[:]
-
-"""
 #This algo does not return ridglet coefficients, but rather the 
 #lines that have been created before the wavelet transform should 
 #occur
@@ -47,7 +20,7 @@ def algorithm3(pos):
     print "Computing 3D FFT..."
     FFTdensity = np.fft.fftn(density)
     
-    
+    #Initializing line's start and end points
     startl, endl = cube.lineParse(size)
 
     print "Creating lines..."
@@ -59,19 +32,19 @@ def algorithm3(pos):
         for j in range(len(lines[0][0])):
             fftLines.append(FFTdensity[lines[i][0][j],lines[i][1][j],lines[i][2][j]])
             
+    #Reshaping the lines
     fftLines = np.reshape(fftLines, (np.shape(lines)[0], np.shape(lines)[2]))
     
     #Apply inverse 1D FFT for each line
     ifftLines = np.hypot(np.real(np.fft.ifft(fftLines)), np.imag(np.fft.ifft(fftLines)))
     
-    #Density contrast    
     print "Density contrast..."
     for i in range(len(ifftLines)):
         ifftLines[i] = (ifftLines[i]-np.mean(ifftLines[i]))/np.mean(ifftLines[i])
 
 
     print "Normalization of dataset..."
-    normFactors, lengths = bnorm.finalNormalization(xArray, yArray, zArray, size, startl, endl)   
+    normFactors, lengths = norm.finalNormalization(xArray, yArray, zArray, size, startl, endl)   
     print (np.shape(normFactors))
     ifftLinesN = normFactors
     for i in range(len(ifftLines)):
@@ -114,12 +87,12 @@ cA, cD = algorithm3(pos)
 #    print "Normalization of dataset..."
 #    #Need normalization later
 #    start, end = bnorm.formatting(startl, endl)
-#    cCube, cStart, cEnd = bnorm.center(start, end, size)
+#    cCube, cStart, cEnd = norm.center(start, end, size)
 #    normFactors = []
 #    lengths = []
 #    count = 0
 #    for i in range(len(cStart)):
-#        factor, length = bnorm.normalize(cStart[i], cEnd[i], cCube, 96, i)
+#        factor, length = norm.normalize(cStart[i], cEnd[i], cCube, 96, i)
 #        normFactors.append(factor)
 #        lengths.append(length)
 #        count += 1
@@ -132,3 +105,27 @@ cA, cD = algorithm3(pos)
 #        normalizedLines.append(normalizedLine)
 #    
 #    print "Done!"
+
+"""
+#Data acquisition
+default_dataname = "pos(1)"
+dataname = raw_input("filename w/o '.npz' (default is '%s')?: " %(default_dataname))
+if dataname == "":
+  dataname = default_dataname
+dataname = "../Data/" + dataname
+dataname +=".npz"
+
+print "Loading %s..." %dataname
+xpos = np.load(dataname)['x']
+ypos = np.load(dataname)['y']
+zpos = np.load(dataname)['z']
+
+
+
+#Rearranges into one 2D array
+pos = np.zeros((3,len(xpos)))
+pos[0,:] = xpos[:]
+pos[1,:] = ypos[:]
+pos[2,:] = zpos[:]
+
+"""
