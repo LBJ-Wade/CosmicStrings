d34e5feae5e31cd0b1242788afc8612a6290cc29
felixvalin <felix.valin@mail.mcgill.ca>
2018-03-22 11:17:12 -0400

    Simplified chooselines(start, end, stepLen)

--- Code/pickLines.py
+++ Code/pickLines.py
@@ -11,38 +11,9 @@ import numpy as np
 def getNbLines(cubeLen):
     resolution = input("Percentage of lines to consider (integer): ")/100.0
     stepLen = np.floor(resolution/1.0)
-    
+
     return stepLen
 
 
 def chooseLines(start, end, stepLen):
-    
-    newStart = []
-    newEnd = []
-    
-    newStart.append(start[0])
-    newEnd.append(end[0])
-    
-    for i in range(start):
-        if i + stepLen <= len(start):
-            newStart.append(start[i+stepLen])
-            newEnd.append(end[i+stepLen])
-    
-    
-    
-    
-    
-    
-    return newStart, newEnd
-
-
-
-
-
-
-
-
-
-
-
-
+    return start[0::stepLen], end[0::stepLen]
