5b4de0876be9d3de64018583a9925e9df0b01d4d
EloiseChakour <eloise.chakour@gmail.com>
2018-04-11 21:49:50 -0400

    Cnaged to fft version

--- Code/normalization.py
+++ Code/normalization.py
@@ -259,7 +259,57 @@ for i in range(10):
 
 
 """
+"""
+test = np.zeros((10, 10, 10))
+tCube = np.zeros((10, 10, 10))
 
+for i in range(10):
+    for j in range(10):
+        for k in range(10):
+            test[i, j, k] = i+j+k
+
+
+for i in range(10):
+    for j in range(10):
+        for k in range(10):
+            tCube[i, j, k] = i + 10*j + 100*k
+
+"""
+
+def normalizeFFT(completeArray, centeredCube):
+    fftArray = np.fft.fftn(completeArray)
+    fftCube = np.fft.fftn(centeredCube)
+    normCube = np.divide(fftArray,fftCube)
+    
+    
+    
+    
+    return normCube
+"""  
+index = normalizeFFT(test, tCube) 
+    
+for i in range(10):
+    for k in range(10):
+        for j in range(10):
+            print (index[i, j, k])
+    
+"""    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 
 
